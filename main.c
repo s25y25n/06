40762752cb2724b6408b0c6341c8a5dcb44ee23d
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-10-12 15:40:10 +0900

    01

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+    void print_star()
+    {
+         int i;
+         for(i=0;i<10;i++)
+           printf("*");
+     }
+     
+    int main(void)
+    {
+        int i;
+        
+        print_star(); 
+        print_star();
+        print_star();
+
+
+  system("PAUSE");	
+  return 0;
+  }
